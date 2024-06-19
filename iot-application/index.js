2e387a8c2d13f73becd258525afbfbde47796ef7
Emilija Cojbasic <ecojbasic@elfak.rs>
2024-06-19 17:14:01 +0200

    Ucitavanje i pokretanje modela, soketi

--- iot-application/index.js
+++ iot-application/index.js
@@ -1,5 +1,7 @@
 const mqtt = require('mqtt');
 const { InfluxDB, Point } = require('@influxdata/influxdb-client');
+const tf = require('tensorflow/lite');
+const io = require('socket.io-client');
 
 const mqttAddress = 'tcp://emqx:1883'
 const clientId = 'iot-application'
@@ -16,6 +18,8 @@ const influxdb = new InfluxDB({ url, token });
 
 const writeApi = influxdb.getWriteApi(organization, bucket);
 
+const socket = io('http://127.0.0.1:5500/');  //live server vscode
+
 const mqttClient = mqtt.connect(mqttAddress, {
     clientId,
     connectTimeout: 4000,
@@ -31,6 +35,19 @@ mqttClient.on('connect', () => {
     })
 })
 
+const path = require('path');
+const modelPath = path.join(__dirname, '..', 'model', 'env_model.tflite');
+const model = new tf.Model(modelPath);
+
+const runModel = (data) => {
+  const input = new Float32Array([data.temperature, data.pressure, data.humidity]);
+  const output = new Float32Array(1); 
+
+  model.predict(input, output);
+
+  return output[0]; 
+};
+
 mqttClient.on('message', (topic, payload) => {
     const json = JSON.parse(payload)
     console.log(`Received Message from '${topic}': `, json)
@@ -44,8 +61,17 @@ mqttClient.on('message', (topic, payload) => {
     writeApi.flush();
 
     // neki poziv ML
+    const prediction = runModel(json);
 
     // emit na socket
+    if (prediction >= 0.5) { // veća verovatnoća da je dobro okruženje (binarni klasifikator)
+      socket.emit('environment_status', { status: 'good', data: json });
+      console.log('Emitting good environment status to web app');
+    } else {
+      socket.emit('environment_status', { status: 'bad', data: json });
+      console.log('Emitting bad environment status to web app');
+    }
+
 })
 
 process.on('exit', () => {
